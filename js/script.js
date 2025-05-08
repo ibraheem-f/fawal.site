4b16451923162a45d492a59491474c77a7dbe9cc
Ibraheem Fawal <ifawal@horizon.csueastbay.edu>
2025-05-07 20:52:36 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,20 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+// THEME TOGGLE HANDLER
+const toggle = document.getElementById('themeSwitch');
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+  document.body.classList.add('dark');
+  toggle.checked = true;
+}
+
+toggle.addEventListener('change', () => {
+  document.body.classList.toggle('dark');
+  if (document.body.classList.contains('dark')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
+});
